a87007aa22088b11d47d22bed3679faf357cc04b
Manoj <manojbk488@gmail.com>
2024-07-15 14:53:53 +0545

    movie app using nodejs

---
+++ routes/moviesRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const moviesController = require('./../controllers/moviesController')
+
+const router = express.Router();  // act as middleware
+
+// router.param('id', (req,res, next, value) => {
+//     console.log("Movie id is: " + value);
+//     next();
+// });
+
+router.param('id', moviesController.checkId);
+
+router.route('/')
+    .get(moviesController.getAllMovies)
+    .post(moviesController.validateBody, moviesController.createMovie);
+
+router.route('/:id')
+    .get(moviesController.getMovie)    
+    .patch(moviesController.updateMovie)
+    .delete(moviesController.deleteMovie);
+
+module.exports = router;
