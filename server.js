f340374c0326c527ff13062088515701267e4375
Manoj <manojbk488@gmail.com>
2024-07-15 15:51:57 +0545

    connect to remote database from Express

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 
@@ -6,6 +7,15 @@ const app = require('./app')
 // console.log(app.get('env'));
 
 console.log(process.env);
+
+mongoose.connect(process.env.CONN_STR, {
+    useNewUrlParser: true
+}).then((conn) => {
+    // console.log(conn);
+    console.log('DB Connection Successful...');
+}).catch((error) => {
+    console.log('Some error has occured.');
+});
 
 // CREATER SERVER   
 const port = process.env.PORT || 3000;
