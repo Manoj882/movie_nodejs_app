a906675846b5b8dd78562fe84e5f85506ba7e349
Manoj <manojbk488@gmail.com>
2024-07-31 06:23:44 +0545

    handling uncaught exception

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 
+process.on('uncaughtException', (err) => {
+    console.log(err.name, err.message);
+    console.log('Unhandled exception occured! Shutting down...');
+    process.exit(1);
+});
+
 const app = require('./app')
 
 // console.log(app.get('env'));
@@ -28,3 +34,8 @@ process.on('unhandledRejection', (err) => {
 
     });
 });
+
+// console.log(x);
+
+
+
